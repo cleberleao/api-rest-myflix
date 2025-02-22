33410dd2abec4a68ad688ad021c0053a16351184
cleberleao <cleberleao_14@hotmail.com>
2025-02-22 10:15:58 -0300

    models

--- myflix/views.py
+++ myflix/views.py
@@ -1,3 +1,10 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+def users(request):
+    if request.method == 'GET':
+        user={
+            'id':1,
+            'nome': 'José'
+        }
+    return JsonResponse(user, json_dumps_params={'ensure_ascii':False})
